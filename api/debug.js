f0cbe67468879a7deb489e471df2eed1d6bd0155
Ghassan Gaidi <leo2574@proton.me>
2026-06-20 18:09:35 +0200

    fix: add explicit functions config in vercel.json for api/bot.js

--- api/debug.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-module.exports = async (req, res) => {
-  const cwd = process.cwd();
-  const apiFiles = fs.existsSync(path.join(cwd, 'api'))
-    ? fs.readdirSync(path.join(cwd, 'api'))
-    : [];
-  res.json({ cwd, apiFiles });
-};
